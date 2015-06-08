c661fcdd70a12a9df21bd045133dd2b99a2812f7
MadHatterTenSix <beastwood2013@gmail.com>
2015-06-08 09:24:30 -0600

    Update palindromic.jl

--- palindromic.jl
+++ palindromic.jl
@@ -3,20 +3,31 @@
 # Author: __MadHatter (alias used on https://www.reddit.com/r/dailyprogrammer)                        
 # Challenge:
 # http://www.reddit.com/r/dailyprogrammer/comments/38yy9s/20150608_challenge_218_easy_making_numbers/
+#
+# Optimizations/improvements by:
+#   SingularInfinity
+#
+# Adjustments to while loop inspired by:
+#   SerJothanChanes
+#   Oops_TryAgain
 
-line = readline() # read integer
-line = line[1:length(line)-1] # remove next line character
-a = line
-b = reverse(line)
-x = BigInt(a)
-y = BigInt(b)
-z = 0
-steps = 0
-while ((z = x + y) / 2) != x
-  a = string(z)
-  b = reverse(a)
+function f()
+  line = readline() |> chomp # read integer
+  a = line
+  b = reverse(line)
   x = BigInt(a)
   y = BigInt(b)
-  steps = steps + 1
+  z = 0
+  steps = 0
+  while a != b
+    z = x + y
+    a = string(z)
+    b = reverse(a)
+    x = BigInt(a)
+    y = BigInt(b)
+    steps = steps + 1
+  end
+  @printf "%s gets palindromic after %d steps: %d\n" line steps x
 end
-@printf "%s gets palindromic after %d steps: %d\n" line steps x
+
+f()
